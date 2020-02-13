2c3cdcabc2ee3f9e712939aceb46e929b3efd1d1
admin1 <admin1@HZ352013.H015112.NET>
2020-02-13 14:05:57 +0100

    Clase 2

---
+++ P1P2_Proyect/src/main/java/P1P2_Proyect/Clase2.java
@@ -0,0 +1,9 @@
+package P1P2_Proyect;
+
+public class Clase2 {
+
+	public void Welcome() {
+		System.out.println("Buenos dias");
+	}
+	
+}
---
+++ P1P2_Proyect/src/test/java/P1P2_Proyect/TestClase2.java
@@ -0,0 +1,16 @@
+package P1P2_Proyect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClase2 {
+
+	private Clase2 prueba = new Clase2();
+	
+	@Test
+	public void testWelcome() {
+		prueba.Welcome();
+	}
+
+}
